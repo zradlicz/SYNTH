2a9c13cbafc3b967c44d0cdcc8f6ff3a60401d60
Zach Radlicz <zach.radlicz1@toro.com>
2024-11-29 13:48:35 -0600

    removed reference

--- src/main.c
+++ src/main.c
@@ -9,16 +9,16 @@ static DoubleBuffer_T audio_buffer;
 static Oscillator_T osc;
 
 void audio_loop() {
-    double_buffer_init(&audio_buffer);
-    oscillator_init(&osc, SAMPLE_RATE, WAVEFORM_SQUARE);
+    double_buffer_init(audio_buffer);
+    oscillator_init(osc, SAMPLE_RATE, WAVEFORM_SQUARE);
 
     oscillator_set_frequency(&osc, 440.0);
     oscillator_set_amplitude(&osc, 20000);
 
     while (true) {
-        int16_t* inactive_buffer = double_buffer_get_inactive(&audio_buffer);
-        oscillator_generate(&osc, inactive_buffer, BUFFER_SIZE);
-        double_buffer_swap(&audio_buffer);
+        int16_t* inactive_buffer = double_buffer_get_inactive(audio_buffer);
+        oscillator_generate(osc, inactive_buffer, BUFFER_SIZE);
+        double_buffer_swap(audio_buffer);
 
         // Replace this line with non-blocking ALSA output
         printf("Audio buffer ready\n");
