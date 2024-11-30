72c74305fe76422815e4847aec1bf7624bdd5305
Zach Radlicz <zach.radlicz1@toro.com>
2024-11-30 17:03:03 -0600

    fixed segfault

--- src/buffer.c
+++ src/buffer.c
@@ -7,10 +7,12 @@ struct DoubleBuffer_S {
 };
 
 DoubleBuffer_T DoubleBuffer_New(void) {
+    static struct DoubleBuffer_S db;
+    DoubleBuffer_T me = &db;
     DoubleBuffer_T db;
-    memset(db->active_buffer, 0, sizeof(db->active_buffer));
-    memset(db->inactive_buffer, 0, sizeof(db->inactive_buffer));
-    return db;
+    memset(me->active_buffer, 0, sizeof(me->active_buffer));
+    memset(me->inactive_buffer, 0, sizeof(me->inactive_buffer));
+    return me;
 }
 
 void DoubleBuffer_GetActive(DoubleBuffer_T db, int16_t* data, size_t size) {
