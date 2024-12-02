a08a4e06b09f16085346684a8e8180b10145392d
Zach Radlicz <zach.radlicz1@toro.com>
2024-12-01 22:57:12 -0600

    adjusted

--- src/output.c
+++ src/output.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include "buffer.h"
 
 //output to file
 void Output_File(int16_t * buffer, size_t size) 
@@ -7,7 +8,7 @@ void Output_File(int16_t * buffer, size_t size)
     //open file
     FILE *file = fopen("output.wav", "w");
     //write sample to file
-    for(size_t i = 0; i < size; i++)
+    for(size_t i = 0; i < (size/sizeof(int16_t)); i++)
     {
         fprintf(file, "%d", buffer[i]);
     }
